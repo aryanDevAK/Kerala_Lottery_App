bcedc6c51e775f77a962f0da9d8a87094acbb48f
AK-Developers <aryankhatri9811@gmail.com>
2023-09-23 01:29:26 +0530

    Kerala Lottery

--- app/src/main/java/com/example/keralalottery/MainActivity.java
+++ app/src/main/java/com/example/keralalottery/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.keralalottery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+EditText text1, text2;
+Button btn1;
+TextView text3;
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.button);
+        text1 = findViewById(R.id.editText);
+        text2 = findViewById(R.id.editText2);
+        text3 = findViewById(R.id.textView4);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Activity2.class);
+                startActivity(i);
+            }
+        });
+
+        text3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),SignUp.class);
+                startActivity(i);
+            }
+        });
     }
 }

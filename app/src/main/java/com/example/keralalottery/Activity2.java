c2ee18cf007bca5fe07212387c738bbc6bd6743e
AK-Developers <aryankhatri9811@gmail.com>
2023-09-23 16:03:51 +0530

    Kerala Lottery

--- app/src/main/java/com/example/keralalottery/Activity2.java
+++ app/src/main/java/com/example/keralalottery/Activity2.java
@@ -3,6 +3,7 @@ package com.example.keralalottery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater() .inflate(R.menu.bottom_navigation_menu, menu);
+        return true;
     }
 }

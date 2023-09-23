99525108f54ffe170d65049b39d549c2fd6a7758
AK-Developers <aryankhatri9811@gmail.com>
2023-09-24 00:16:40 +0530

    Kerala Lottery

--- app/src/main/java/com/example/keralalottery/Activity2.java
+++ app/src/main/java/com/example/keralalottery/Activity2.java
@@ -2,12 +2,18 @@ package com.example.keralalottery;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class Activity2 extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    ListView listView;
+    ArrayList<Lottery> lotteryArrayList;
+    private static MyCustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,28 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+
+        //Adapter of List
+        listView = findViewById(R.id.lotteryListView);
+
+        //Data Source
+        lotteryArrayList = new ArrayList<>();
+
+        Lottery lottery1 = new Lottery("10",R.drawable.rupees_10);
+        Lottery lottery2 = new Lottery("50",R.drawable.rupees_50);
+        Lottery lottery3 = new Lottery("70",R.drawable.rupees_70);
+        Lottery lottery4 = new Lottery("80",R.drawable.rupees_80);
+        Lottery lottery5 = new Lottery("500",R.drawable.rupees_500);
+
+        lotteryArrayList.add(lottery1);
+        lotteryArrayList.add(lottery2);
+        lotteryArrayList.add(lottery3);
+        lotteryArrayList.add(lottery4);
+        lotteryArrayList.add(lottery5);
+
+        //Adapter
+        adapter = new MyCustomAdapter(lotteryArrayList,getApplicationContext());
+        listView.setAdapter(adapter);
     }
 }
 
--- app/src/main/java/com/example/keralalottery/MyCustomAdapter.java
+++ app/src/main/java/com/example/keralalottery/MyCustomAdapter.java
@@ -1,12 +1,17 @@
 package com.example.keralalottery;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -23,5 +28,39 @@ public class MyCustomAdapter extends ArrayAdapter<Lottery> {
     private static class MyViewHolder{
         TextView lotteryPrice;
         ImageView lotteryImage;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Lottery lottery = getItem(position);
+
+        //Inflating the View
+        MyViewHolder myViewHolder;
+        final View result;
+        if (convertView == null){
+            myViewHolder = new MyViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(
+                    R.layout.lottery_list_layout,
+                    parent,
+                    false
+            );
+
+        //Finding the Views
+            myViewHolder.lotteryPrice = (TextView) convertView.findViewById(R.id.price);
+            myViewHolder.lotteryImage = (ImageView) convertView.findViewById(R.id.imageView3);
+
+            result = convertView;
+
+            convertView.setTag(myViewHolder);
+        }else {
+            myViewHolder = (MyViewHolder) convertView.getTag();
+            result = convertView;
+        }
+        myViewHolder.lotteryPrice.setText(lottery.getLotteryPrize());
+        myViewHolder.lotteryImage.setImageResource(lottery.getLotteryImage());
+
+        return result;
     }
 }
